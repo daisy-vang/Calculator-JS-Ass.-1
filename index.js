abe80da50f927c897b658b8e52fa13a167008ce7
daisy-vang <daisy_jane.vang24@student.cart.org>
2025-01-30 09:15:05 -0800

    deleted files and removed the js coding

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const display = document.getElementById("display");
-function appendToDisplay(input) {
+
+function appendToDisplay(input){
     display.value += input;
 }
 
 function clearDisplay() {
-    display.value = "";
+
 }
 
 function calculate() {
-    display.value = 
+    
 }
